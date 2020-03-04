85135b4918a208ddebe55318b2090446de354143
Dimitri Ostapenko <dosta@me.com>
2020-03-04 18:25:49 +0100

    order seeding script revised: 5 items/order

--- db/seeds/03_carmodel_orders.rb
+++ db/seeds/03_carmodel_orders.rb
@@ -3,9 +3,18 @@
 require_relative '../../config/environment'
 require 'csv'
 
+ITEMS_PER_ORDER = 5
+$client = Client.find(405)  #! Client Table dependent
+
 puts "About to seed orders table with carmodel orders (from Susanna)"
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'carmodel_orders.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1', :col_sep => ',')
+
+def create_order(prod_and_qty)
+  order = $client.orders.new()
+  order.build_placements_with_product_ids_and_quantities?(prod_and_qty)
+  order.save!
+end
 
 # Destroy all orders with dependent Placements
 Order.destroy_all
@@ -17,9 +26,8 @@ Dir.glob("#{PPOS_PATH}/*.pdf").each { |file| File.delete(file)}
 # Reset all inventory to 0:
 Product.update_all(quantity: 0)
 
-client = Client.find(405)  #! Client Table dependent
 
-num =0
+num =0; prod_and_qty = []
 csv.each do |row|
     refcode = row[0].strip rescue nil
     quantity = row[1].to_i rescue nil
@@ -41,11 +49,13 @@ csv.each do |row|
     
     prod = Product.find_by(ref_code: new_refcode)
     if prod
-      puts "# #{num} Ref code: #{row[0]} => #{new_refcode} qty: #{quantity} : creating order"
-      order = client.orders.new()
-      prod_and_qty = [[prod.id,quantity]]
-      order.build_placements_with_product_ids_and_quantities?(prod_and_qty)
-      order.save!
+      puts "# #{num} Ref code: #{row[0]} => #{new_refcode} qty: #{quantity} : adding to order"
+      prod_and_qty.push([prod.id,quantity])
+      if prod_and_qty.size > ITEMS_PER_ORDER-1
+         create_order(prod_and_qty)
+         prod_and_qty = [] 
+         puts "*** Next Order" 
+      end
     else
       puts "** Product NOT found in DB #{new_refcode}"
     end
@@ -54,3 +64,6 @@ csv.each do |row|
     puts "#{row[0]} is not recognized as product code"
   end
 end
+# Remaining products
+create_order(prod_and_qty)
+
